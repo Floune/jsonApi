d58f3c113e8938bf4bcf5164c328dbfef961d72e
floune <robin.sangla@live.fr>
2018-08-29 17:18:50 +0200

    final commit

--- tests/contactList.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../routes')
-describe('Test the contact display patch', () => {
-    test('It should response the POST method', () => {
-        return request(app).post("/api/contacts").then(response => {
-            expect(response.statusCode).toBe(200)
-        })
-    });
-})
